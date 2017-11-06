751a4c177f0c732cbaf2d36d0542e348a37699f9
yberman <yberman@users.noreply.github.com>
2017-11-06 05:27:39 -0500

    corrected apt-get

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,10 +1,15 @@
 set -x
 set -e
 
+
+apt-get update
+
 echo "apt-getting packages"
 while read -r package_name
 do
-    sudo apt-get install "$package_name"
+    apt-get -y install "$package_name"
 done < apt-get.txt
+
+apt-get upgrade
 
 touch DONE
